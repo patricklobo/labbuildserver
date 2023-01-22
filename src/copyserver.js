37e84a281a80db4c17d42a65c3d462fcc1315500
Patrick Lobo <patricklobofilho@gmail.com>
2023-01-21 21:51:25 -0300

    lobo

--- src/copyserver.js
+++ src/copyserver.js
@@ -7,22 +7,17 @@ const init = async () =>{
     try {
         const questions = [
             {
-              type: 'text',
-              name: 'dish',
-              message: 'Do you like pizza?'
+              type: 'number',
+              name: 'dev',
+              message: 'Qual disco deseja instalar?'
             },
-            {
-              type: prev => prev == 'pizza' ? 'text' : null,
-              name: 'topping',
-              message: 'Name a topping'
-            }
           ];
-
+          
         let resp = await UTIL.cmd("sudo fdisk -l | grep \"Disk /dev/sd\"");
         let list = resp.split("\n").filter( i => i);
-        let devs = list.map( i => {
+        let devs = list.map( (i, index) => {
             let _devs = i.split(" ");
-            let dev = `${_devs[2]} ${_devs[3]} -> ${_devs[1]}`;
+            let dev = `[${(index + 1)}] -> ${_devs[2]} ${_devs[3]} -> ${_devs[1]}`;
             return dev;
         })
         const response = await prompts(questions);
