ca33dd4b6a1b770d6dc6fa59271db6da7e098c41
Patrick Lobo <patricklobofilho@gmail.com>
2023-01-21 22:24:48 -0300

    lobo

--- src/copyserver.js
+++ src/copyserver.js
@@ -1,29 +1,29 @@
+import { UTIL } from "./util";
+const prompts = require("prompts");
 
-import { UTIL } from './util';
-const prompts = require('prompts');
-
-
-const init = async () =>{
+const init = async () => {
     try {
         const questions = [
             {
-              type: 'number',
-              name: 'dev',
-              message: 'Digite o número do disco deseja instalar:'
+                type: "number",
+                name: "dev",
+                message: "Digite o número do disco deseja instalar:",
             },
-          ];
-          
-        let resp = await UTIL.cmd("sudo fdisk -l | grep \"Disk /dev/sd\"");
-        let list = resp.split("\n").filter( i => i);
-        let _devs = list.map( (i, index) => {
+        ];
+
+        let resp = await UTIL.cmd('sudo fdisk -l | grep "Disk /dev/sd"');
+        let list = resp.split("\n").filter((i) => i);
+        let _devs = list.map((i, index) => {
             let _devs = i.split(" ");
-            return _devs[1].substring(0,_devs[1].length -1);
-        })
-        let devs = list.map( (i, index) => {
+            return _devs[1].substring(0, _devs[1].length - 1);
+        });
+        let devs = list.map((i, index) => {
             let _devs = i.split(" ");
-            let dev = `[${(index + 1)}] -> ${_devs[2]} ${_devs[3]} -> ${_devs[1]}`;
+            let dev = `[${index + 1}] -> ${_devs[2]} ${_devs[3]} -> ${
+                _devs[1]
+            }`;
             return dev;
-        })
+        });
         console.clear();
         console.log();
         console.log("LISTA DE DISCOS");
@@ -31,9 +31,21 @@ const init = async () =>{
         console.log(devs.join("\n"));
         console.log();
         const response = await prompts(questions);
-        let disco = _devs[response.dev - 1];
-        if(disco){
-            console.log("Disco selecionado: ", disco);
+        let index = response.dev - 1;
+        let disco = _devs[index];
+        if (disco) {
+            console.clear();
+            console.log();
+            console.log("Disco selecionado: ", devs[index]);
+            const response = await prompts({
+                type: "confirm",
+                name: "value",
+                message: "Tem certeza que deseja prosseguir?",
+                initial: true,
+            });
+            if (response.value) {
+                console.log("SAL");
+            }
         } else {
             console.log("Disco selecionado inválido!");
         }
@@ -42,6 +54,6 @@ const init = async () =>{
     } catch (error) {
         console.log("erro", error);
     }
-}
+};
 
 init();
