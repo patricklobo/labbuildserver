a7c543e5fa34fec24ff9805673fe414bef539ab6
Patrick Lobo <patricklobofilho@gmail.com>
2023-01-21 21:46:03 -0300

    lobo

--- src/copyserver.js
+++ src/copyserver.js
@@ -1,9 +1,29 @@
 
 import { UTIL } from './util';
+const prompts = require('prompts');
 
 
 const init = async () =>{
     try {
+        const questions = [
+            {
+              type: 'text',
+              name: 'username',
+              message: 'What is your GitHub username?'
+            },
+            {
+              type: 'number',
+              name: 'age',
+              message: 'How old are you?'
+            },
+            {
+              type: 'text',
+              name: 'about',
+              message: 'Tell something about yourself',
+              initial: 'Why should I?'
+            }
+          ];
+          
         let resp = await UTIL.cmd("sudo fdisk -l | grep \"Disk /dev/sd\"");
         let list = resp.split("\n").filter( i => i);
         let devs = list.map( i => {
@@ -11,6 +31,8 @@ const init = async () =>{
             let dev = `${_devs[2]} ${_devs[3]} -> ${_devs[1]}`;
             return dev;
         })
+        const response = await prompts(questions);
+        console.log(response)
         console.log(devs);
     } catch (error) {
         console.log("erro", error);
