55ef69cabf4edd3d2dd4f5df3668a59e36cd163f
Patrick Lobo <patricklobofilho@gmail.com>
2023-01-21 21:41:34 -0300

    lobo

--- src/copyserver.js
+++ src/copyserver.js
@@ -5,7 +5,13 @@ import { UTIL } from './util';
 const init = async () =>{
     try {
         let resp = await UTIL.cmd("sudo fdisk -l | grep \"Disk /dev/sd\"");
-        console.log(resp.split("\n"));
+        let list = resp.split("\n").filter( i => i);
+        let devs = list.map( i => {
+            let _devs = i.split(" ");
+            let dev = `${_devs[2]} ${_devs[3]} -> ${1}`;
+            return dev;
+        })
+        console.log(devs);
     } catch (error) {
         console.log("erro", error);
     }
